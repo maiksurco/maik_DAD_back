9fdfb956251a6437f6de65762854bbed212dd46a
maiksurco <rickisurco@upeu.edu.pe>
2023-10-09 09:11:55 -0500

    auth conec

--- ms-auth/src/main/java/maik/example/msauth/entity/AuthUser.java
+++ ms-auth/src/main/java/maik/example/msauth/entity/AuthUser.java
@@ -1,23 +1,20 @@
 package maik.example.msauth.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class AuthUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- ms-auth/src/main/java/maik/example/msauth/security/JwtProvider.java
+++ ms-auth/src/main/java/maik/example/msauth/security/JwtProvider.java
@@ -2,11 +2,12 @@ package maik.example.msauth.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import jakarta.annotation.PostConstruct;
+
 import maik.example.msauth.entity.AuthUser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
